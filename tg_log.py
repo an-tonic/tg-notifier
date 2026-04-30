15ef30cb669ce9e19c6033f159565fd51ec26fd3
serdy <serdyukovantoniy@gmail.com>
2026-04-30 13:46:46 +0300

    Correct file

--- tg_log.py
+++ tg_log.py
@@ -1,16 +1,13 @@
 import os
-
+import inspect
 import requests
 
 
 def log_to_tg(message):
-    """Topic should be a unique string to allow identification of source of the message"""
-
+    caller_file = os.path.abspath(inspect.stack()[1].filename)
     ip = os.popen("hostname -I").read().strip().split()[0]
-    file_path = os.path.abspath(__file__)
-    topic = f"{ip}/{file_path}"
-    print(topic)
+    topic = f"{ip}/{caller_file}"
     requests.post("http://127.0.0.1:12345/notify",
-                  json={"message": f"Message from: {ip} on this file {file_path}:\n {message}",
+                  json={"message": f"[{ip}] {caller_file}:\n\n{message}",
                         "topic": topic},
                   timeout=5)
